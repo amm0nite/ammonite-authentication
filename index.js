962d38882fd6df1d61d8be76154d62e6d8d7c491
amm0nite <ammonite.myfox@gmail.com>
2020-06-24 23:04:29 +0200

    Use the new api url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Authorization {
 
     query(accessToken) {
         let options = {
-            uri: 'https://security.amnt.fr/user',
+            uri: 'https://security.amnt.fr/auth/user',
             headers: {
                 'Authorization': 'Bearer ' + accessToken
             },
