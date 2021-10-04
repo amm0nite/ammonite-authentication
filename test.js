5110510c3f23ee2ec6304244f5886f394b90a790
amm0nite <ammonite.myfox@gmail.com>
2021-10-04 23:20:47 +0200

    Improve test script

--- test.js
+++ test.js
@@ -1,40 +1,73 @@
-// NODE_DEBUG=ammonite-authentication node test.js
-
-const axios = require('axios');
+const assert = require('assert');
 const express = require('express');
-const app = express();
+const axios = require('axios');
 
-const port = 3000;
-const baseUrl = `http://localhost:${port}`;
+const Authentication = require('./index.js');
 
-const Authentication = require('./index');
-const auth = new Authentication(`${baseUrl}/user`);
+describe('middleware', function () {
+    const port = 3000;
+    const baseUrl = `http://localhost:${port}`;
 
-app.get('/user', (req, res) => {
-    //return res.status(404).json({ message: 'not found' });
-    return res.json({
-        "login": "bob",
-        "id": 11,
+    const login = 'testuser';
+    const id = 42;
+
+    let auth = null;
+    let server = null;
+    let app = null;
+
+    beforeEach(function (next) {
+        auth = new Authentication(`${baseUrl}/user200`);
+
+        app = express();
+
+        app.get('/user200', (req, res) => {
+            return res.json({ login, id });
+        });
+
+        app.get('/user401', (req, res) => {
+            return res.status(401).json({ message: "bad token" });
+        });
+
+        app.get('/', auth.middleware(), (req, res) => {
+            res.status(200).json(req.user);
+        });
+
+        server = app.listen(port, () => {
+            return next();
+        });
     });
-});
 
-app.get('/', auth.middleware(), (req, res) => {
-    res.status(200).json(req.user);
-});
-
-const server = app.listen(port, function () {
-    console.log('Example app listening on port ' + port);
-
-    const token = 'helloworld';
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-
-    axios.get(baseUrl, options).then((res) => {
-        console.log(res.data);
-        auth.close();
+    afterEach(function () {
         server.close();
-    }).catch((err) => {
-        console.log(err);
         auth.close();
-        server.close();
+    });
+
+    it('should provide user details', async function () {
+        const token = 'helloworld';
+        const options = { headers: { Authorization: `Bearer ${token}` } };
+
+        const firstResponse = await axios.get(baseUrl, options);
+        assert.equal(firstResponse.data.login, login);
+        assert.equal(firstResponse.data.id, id);
+        assert.equal(firstResponse.data.cached, false);
+
+        const secondResponse = await axios.get(baseUrl, options);
+        assert.equal(secondResponse.data.login, login);
+        assert.equal(secondResponse.data.id, id);
+        assert.equal(secondResponse.data.cached, true);
+    });
+
+    it('should return unauthorized', async function () {
+        auth.getUserURL = `${baseUrl}/user401`;
+
+        const token = 'helloworld';
+        const options = { headers: { Authorization: `Bearer ${token}` } };
+
+        try {
+            const response = await axios.get(baseUrl, options);
+            assert.fail("Should fail as unauthorized");
+        } catch (err) {
+            assert.equal(err.response.status, 401);
+        }
     });
 });
